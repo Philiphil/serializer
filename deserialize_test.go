4f3991632e18c1e2f58217431434e2db7f5e352f
Théo Sorriaux <theo@augmenteo.com>
2023-11-22 14:04:16 +0100

    last stable version

--- deserialize_test.go
+++ deserialize_test.go
@@ -2,11 +2,9 @@ package serializer
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
 )
 
-// basic struct
 func TestSerializer_Deserialize(t *testing.T) {
 	s := NewSerializer(JSON)
 	serialized, err := s.Serialize(test, "test")
@@ -19,241 +17,6 @@ func TestSerializer_Deserialize(t *testing.T) {
 		panic("!")
 	}
 
-}
-
-// nested struct
-func TestSerializer_Deserialize2(t *testing.T) {
-	test2 := Recursive{
-		Hidden{1, 2},
-		Hidden{3, 4},
-	}
-	expected2 := Recursive{
-		Hidden{1, 0},
-		Hidden{0, 0},
-	}
-
-	s := NewSerializer(JSON)
-	serialized, err := s.Serialize(test2, "test")
-	if err != nil {
-		panic(err)
-	}
-	o := Recursive{}
-	err = s.Deserialize(serialized, &o)
-	if o != expected2 {
-		fmt.Println(o)
-		fmt.Println(expected2)
-		panic("!")
-	}
-
-}
-
-// slice
-func TestSerializer_Deserialize3(t *testing.T) {
-	test2 := []Recursive{
-		{
-			Hidden{1, 2},
-			Hidden{3, 4},
-		},
-	}
-	expected2 := []Recursive{
-		{
-			Hidden{1, 0},
-			Hidden{0, 0},
-		},
-	}
-	s := NewSerializer(JSON)
-	serialized, err := s.Serialize(test2, "test")
-	if err != nil {
-		panic(err)
-	}
-	o := []Recursive{}
-	err = s.Deserialize(serialized, &o)
-	if o[0] != expected2[0] {
-		fmt.Println(o)
-		fmt.Println(expected2)
-		panic("!")
-	}
-
-}
-
-// ptr to struct
-func TestSerializer_Deserialize4(t *testing.T) {
-	test1 := new(Hidden)
-	expected1 := new(Hidden)
-	test1.Test0 = 1
-	test1.Test1 = 1
-	expected1.Test0 = 1
-	expected1.Test1 = 0
-
-	s := NewSerializer(JSON)
-	serialized, err := s.Serialize(test1, "test")
-	if err != nil {
-		panic(err)
-	}
-	o := new(Hidden)
-	err = s.Deserialize(serialized, &o)
-	if *o != *expected1 {
-		panic("!")
-	}
-
-}
-
-// slice of ptr
-func TestSerializer_Deserialize5(t *testing.T) {
-	test2 := []*Recursive{
-		{
-			Hidden{1, 2},
-			Hidden{3, 4},
-		},
-	}
-	expected2 := []*Recursive{
-		{
-			Hidden{1, 0},
-			Hidden{0, 0},
-		},
-	}
-	s := NewSerializer(JSON)
-	serialized, err := s.Serialize(test2, "test")
-	if err != nil {
-		panic(err)
-	}
-	o := []*Recursive{}
-	err = s.Deserialize(serialized, &o)
-	if *o[0] != *expected2[0] {
-		fmt.Println(*o[0])
-		fmt.Println(*expected2[0])
-		panic("!")
-	}
-
-}
-
-// ptr slice
-func TestSerializer_Deserialize6(t *testing.T) {
-	test2 := new([]Recursive)
-	*test2 = []Recursive{
-		{
-			Hidden{1, 2},
-			Hidden{3, 4},
-		},
-	}
-	expected2 := []Recursive{
-		{
-			Hidden{1, 0},
-			Hidden{0, 0},
-		},
-	}
-	s := NewSerializer(JSON)
-	serialized, err := s.Serialize(test2, "test")
-	if err != nil {
-		panic(err)
-	}
-	o := new([]Recursive)
-	err = s.Deserialize(serialized, o)
-	if (*o)[0] != expected2[0] {
-		fmt.Println(*o)
-		fmt.Println(expected2)
-		panic("!")
-	}
-}
-
-// struct with ptr
-func TestSerializer_Deserialize7(t *testing.T) {
-	test2 := new([]Recursive)
-	*test2 = []Recursive{
-		{
-			Hidden{1, 2},
-			Hidden{3, 4},
-		},
-	}
-	expected2 := []Recursive{
-		{
-			Hidden{1, 0},
-			Hidden{0, 0},
-		},
-	}
-	s := NewSerializer(JSON)
-	serialized, err := s.Serialize(test2, "test")
-	if err != nil {
-		panic(err)
-	}
-	o := new([]Recursive)
-	err = s.Deserialize(serialized, o)
-	if (*o)[0] != expected2[0] {
-		fmt.Println(*o)
-		fmt.Println(expected2)
-		panic("!")
-	}
-}
-
-// struct w/ ptr & ptr to nested
-func TestSerializer_Deserialize8(t *testing.T) {
-	intValue := 42
-
-	test := Ptr{
-		Test0: 1,
-		Test1: &intValue,
-		Test2: &Hidden{2, 3},
-		Test3: &intValue,
-		Test4: &Hidden{2, 3},
-	}
-
-	expected := Ptr{
-		Test0: 1,
-		Test1: &intValue,
-		Test2: &Hidden{2, 0},
-		Test3: nil,
-		Test4: nil,
-	}
-
-	s := NewSerializer(JSON)
-	serialized, err := s.Serialize(test, "test")
-	if err != nil {
-		panic(err)
-	}
-
-	var o Ptr
-	err = s.Deserialize(serialized, &o)
-	if !reflect.DeepEqual(o, expected) {
-		fmt.Println(o)
-		fmt.Println(expected)
-		panic("Test failed!")
-	}
-}
-
-// map[any] to map[typed]
-func TestSerializer_Deserialize9(t *testing.T) {
-	test1 := make(map[string]any)
-	test1["test"] = test
-	s := NewSerializer(JSON)
-	serialized, err := s.Serialize(test1, "test")
-	if err != nil {
-		panic(err)
-	}
-	o := make(map[string]Test)
-	err = s.Deserialize(serialized, &o)
-	if o["test"] != testDeserializedResult {
-		fmt.Println(o["test"])
-		fmt.Println(testDeserializedResult)
-		panic("!")
-	}
-}
-
-// map[any] to map[typed]
-func TestSerializer_Deserialize10(t *testing.T) {
-	test1 := make(map[string]Test)
-	test1["test"] = test
-	s := NewSerializer(JSON)
-	serialized, err := s.Serialize(test1, "test")
-	if err != nil {
-		panic(err)
-	}
-	o := make(map[string]Test)
-	err = s.Deserialize(serialized, &o)
-	if o["test"] != testDeserializedResult {
-		fmt.Println(o["test"])
-		fmt.Println(testDeserializedResult)
-		panic("!")
-	}
 }
 
 func TestSerializer_MergeObjects(t *testing.T) {
@@ -304,4 +67,44 @@ func TestSerializer_DeserializeAndMerge(t *testing.T) {
 		panic("!")
 	}
 
+}
+
+type MyStruct struct {
+	Name  string `json:"name" group:"group1"`
+	Age   int    `json:"age" group:"group2"`
+	Email string `json:"email"`
+}
+
+func main() {
+	// Create an instance of Serializer
+	mySerializer := NewSerializer(JSON)
+
+	// Data to serialize
+	dataToSerialize := MyStruct{
+		Name:  "John Doe",
+		Age:   30,
+		Email: "john@example.com",
+	}
+
+	// Serialize data to JSON with the "group1" group
+	serializedData, err := mySerializer.Serialize(dataToSerialize, "json", "group1")
+	if err != nil {
+		fmt.Println("Serialization error:", err)
+		return
+	}
+
+	fmt.Println("Serialized Data:", serializedData)
+
+	// New structure for deserialization
+	var deserializedData MyStruct
+
+	// Deserialize the data
+	err = mySerializer.Deserialize(serializedData, &deserializedData)
+	if err != nil {
+		fmt.Println("Deserialization error:", err)
+		return
+	}
+
+	// Display the deserialized data
+	fmt.Printf("Deserialized Data: %+v\n", deserializedData)
 }
--- filter.go
+++ filter.go
@@ -1,120 +1,33 @@
 package serializer
 
 import (
-	"fmt"
 	"reflect"
 	"strings"
 )
 
-func isFieldIncluded(field reflect.StructField, groups []string) bool {
-	if len(groups) == 0 {
-		return true //No filtration then
-	}
-
-	tag := field.Tag.Get("group")
-	if tag == "" {
-		return false
-	}
-
-	groupList := strings.Split(tag, ",")
-	for _, group := range groups {
-		for _, g := range groupList {
-			if group == g {
-				return true
-			}
-		}
-	}
-
-	return false
-}
-
-func isFieldExported(field reflect.StructField) bool {
-	return field.PkgPath == ""
-}
-
 func isStruct(t reflect.Type) bool {
-	return t.Kind() == reflect.Struct || (t.Kind() == reflect.Ptr && t.Elem().Kind() == reflect.Struct)
-}
-
-func isList(t reflect.Type) bool {
-	return t.Kind() == reflect.Slice || t.Kind() == reflect.Array || (t.Kind() == reflect.Ptr && (t.Elem().Kind() == reflect.Slice || t.Elem().Kind() == reflect.Array))
-}
-func isMap(t reflect.Type) bool {
-	return t.Kind() == reflect.Map || (t.Kind() == reflect.Ptr && t.Elem().Kind() == reflect.Map)
-}
-
-func assignFieldValue(field reflect.StructField, destValue reflect.Value, srcValue reflect.Value) {
-	if field.Type == srcValue.Type() {
-		destValue.Set(srcValue)
-	} else if field.Type.AssignableTo(srcValue.Type()) {
-		destValue.Set(srcValue)
-	} else if field.Type.Kind() == reflect.Ptr && field.Type.Elem() == srcValue.Type() {
-		newPtr := reflect.New(field.Type.Elem())
-		newPtr.Elem().Set(srcValue)
-		destValue.Set(newPtr)
-	} else if isStruct(srcValue.Type()) {
-		destFieldType := destValue.Type()
-		if destFieldType.Kind() == reflect.Ptr {
-			destFieldType = destFieldType.Elem()
-		}
-
-		if destFieldType.Kind() == reflect.Struct {
-			destValueConverted := reflect.New(destFieldType).Elem()
-
-			for i := 0; i < destFieldType.NumField(); i++ {
-				destField := destFieldType.Field(i)
-				srcFieldValue := srcValue
-				if srcFieldValue.Type().Kind() == reflect.Ptr {
-					srcFieldValue = srcFieldValue.Elem()
-				}
-
-				srcFieldValue = srcFieldValue.FieldByName(destField.Name)
-				destFieldValue := destValueConverted.Field(i)
-
-				if isStruct(srcFieldValue.Type()) && destFieldValue.Kind() == reflect.Struct {
-					assignFieldValue(destField, destFieldValue, srcFieldValue)
-				} else {
-					assignFieldValue(destField, destFieldValue, srcFieldValue)
-				}
-			}
-
-			destValue.Set(destValueConverted)
-		} else {
-			destValue.Set(srcValue.Convert(destFieldType))
-		}
-	} else {
-		if !srcValue.Type().ConvertibleTo(destValue.Type()) {
-			fmt.Printf("Type conversion not supported from %v to %v\n", srcValue.Type(), destValue.Type())
-			panic("!")
-		}
-		destValue.Set(srcValue.Convert(destValue.Type()))
-	}
+	return t.Kind() == reflect.Struct
 }
 
 func filterByGroups[T any](obj T, groups ...string) T {
 	value := reflect.ValueOf(obj)
 	elemType := value.Type()
+	if !elementFilterable(elemType) {
+		//panic ou log ?
+		return obj
+	}
 
 	if isStruct(elemType) {
-		return filterByGroups_struct(obj, groups...)
-	}
-	if isList(elemType) {
-		return filterByGroups_slice(obj, groups...)
-	}
-	if isMap(elemType) {
-		return filterByGroups_map(obj, groups...)
+		return filterStructRecursive(obj, groups...)
+	} else if elemType.Kind() == reflect.Map {
+		//	return filterMap(obj, groups...)
 	}
 	return obj
 }
 
-func filterByGroups_struct[T any](obj T, groups ...string) T {
+func filterStructRecursive[T any](obj T, groups ...string) T {
 	value := reflect.ValueOf(obj)
 	elemType := value.Type()
-
-	if elemType.Kind() == reflect.Ptr {
-		elemType = elemType.Elem()
-		value = value.Elem()
-	}
 
 	var newFields []reflect.StructField
 
@@ -123,6 +36,7 @@ func filterByGroups_struct[T any](obj T, groups ...string) T {
 		if isFieldExported(field) && isFieldIncluded(field, groups) {
 			fieldValue := value.Field(i)
 
+			// Appliquer la fonction récursivement si le champ est de type structure
 			if isStruct(field.Type) {
 				filteredElem := filterByGroups(fieldValue.Interface(), groups...)
 				newFields = append(newFields, reflect.StructField{
@@ -142,73 +56,74 @@ func filterByGroups_struct[T any](obj T, groups ...string) T {
 	for i, field := range newFields {
 		fieldName := field.Name
 		fieldValue := value.FieldByName(fieldName)
-		newFieldValue := newValue.Field(i)
-		assignFieldValue(field, newFieldValue, fieldValue)
+		newValue.Field(i).Set(fieldValue)
 	}
 
 	return newValue.Interface().(T)
 }
 
-func filterByGroups_slice[T any](obj T, groups ...string) T {
-	value := reflect.ValueOf(obj)
-
-	if value.Kind() == reflect.Ptr {
-		value = value.Elem()
+func elementFilterable(elem reflect.Type) bool {
+	if isStruct(elem) {
+		return true
+	} else if elem.Kind() == reflect.Map {
+		//	return filterMap(obj, groups...)
 	}
-
-	if value.Len() == 0 {
-		return obj
-	}
-
-	firstElem := value.Index(0).Interface()
-
-	filteredFirstElem := filterByGroups(firstElem, groups...)
-
-	newSlice := reflect.MakeSlice(reflect.SliceOf(reflect.TypeOf(filteredFirstElem)), 0, value.Len())
-
-	for i := 0; i < value.Len(); i++ {
-		elem := value.Index(i)
-		filteredElem := filterByGroups(elem.Interface(), groups...)
-		newSlice = reflect.Append(newSlice, reflect.ValueOf(filteredElem))
-	}
-	return newSlice.Interface().(T)
+	return false
 }
 
-func filterByGroups_map[T any](obj T, groups ...string) T {
+func filterStruct[T any](obj T, groups ...string) T {
 	value := reflect.ValueOf(obj)
 	elemType := value.Type()
 
-	if value.Kind() == reflect.Ptr {
-		value = value.Elem()
-		elemType = elemType.Elem()
-	}
+	var newFields []reflect.StructField
 
-	mapType := reflect.MapOf(elemType.Key(), elemType.Elem())
-	mapValue := reflect.MakeMap(mapType)
-
-	iter := value.MapRange()
-
-	for iter.Next() {
-		key := iter.Key()
-		val := iter.Value()
-		filteredVal := filterByGroups(val.Interface(), groups...)
-
-		filteredValValue, ok := filteredVal.(reflect.Value)
-		if !ok {
-			filteredValValue = reflect.ValueOf(filteredVal)
-		}
-
-		if filteredValValue.Type().AssignableTo(elemType.Elem()) {
-			mapValue.SetMapIndex(key, filteredValValue)
-		} else {
-			destType := elemType.Elem()
-			destValue := reflect.New(destType).Elem()
-
-			assignFieldValue(destType.Field(0), destValue.Field(0), filteredValValue)
-
-			mapValue.SetMapIndex(key, destValue)
+	for i := 0; i < elemType.NumField(); i++ {
+		field := elemType.Field(i)
+		if isFieldExported(field) && isFieldIncluded(field, groups) {
+			newFields = append(newFields, field)
 		}
 	}
 
-	return mapValue.Interface().(T)
+	newStructType := reflect.StructOf(newFields)
+	newValue := reflect.New(newStructType).Elem()
+
+	for i, field := range newFields {
+		fieldName := field.Name
+		fieldValue := value.FieldByName(fieldName)
+		newValue.Field(i).Set(fieldValue)
+	}
+
+	return newValue.Interface().(T)
+}
+
+func isTagIncluded(tag string, groups []string) bool {
+	if tag == "" || len(groups) == 0 {
+		return true
+	}
+	groupList := strings.Split(tag, ",")
+	for _, group := range groups {
+		for _, g := range groupList {
+			if group == g {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func isFieldIncluded(field reflect.StructField, groups []string) bool {
+	tag := field.Tag.Get("group")
+	return isTagIncluded(tag, groups)
+}
+
+func isKeyExported(key interface{}, groups []string) bool {
+	if keyStr, ok := key.(string); ok {
+		return isTagIncluded(keyStr, groups)
+	}
+	return true
+}
+
+func isFieldExported(field reflect.StructField) bool {
+	return field.PkgPath == ""
 }
